5631d02cadab71b27426a452731a9979fd6bc317
FranciscoMedranoS <pako.medrano27@gmail.com>
2024-10-19 23:08:26 -0600

    Cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
-import { brotliDecompress } from 'zlib';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Body, Param, Patch, Res } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { LoginUserDto } from "./dto/login-user.dto";
+import { ApiTags } from "@nestjs/swagger";
+import { Response } from "express";
+import { TOKEN_NAME } from "./constants/jwt.constants";
+import { Cookies } from "./decorators/cookies.decorator";
 
-@ApiTags('Auth')
-@Controller('auth')
+@ApiTags("Auth")
+@Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("signup")
-  signup(@Body() createUserDto: CreateUserDto){
-    return this.authService.registerUser(createUserDto)
-  }
-  @Post("login")
-  login(@Body() loginUserDto :LoginUserDto){
-    return this.authService.loginUser(loginUserDto)
-  }
-  @Patch("/:email")
-  updataUser(@Param('email') userEmial: string ,@Body() UpdateUserDto: UpdateUserDto){
-    return this.authService.updateUser(userEmial, UpdateUserDto)
+  signUp(@Body() createUserDto: CreateUserDto) {
+    return this.authService.registerUser(createUserDto);
   }
 
+  @Post("login")
+  async login(
+    @Body() loginUserDto: LoginUserDto,
+    @Res({ passthrough: true }) response: Response,
+    @Cookies() cookies: any
+  ) {
+    const token = await this.authService.loginUser(loginUserDto);
+
+    response.cookie(TOKEN_NAME, token, {
+      httpOnly: false,
+      secure: true,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    return;
+  }
+
+  @Patch("/:email")
+  updateUser(
+    @Param("email") userEmail: string,
+    @Body() updateUserDto: UpdateUserDto
+  ) {
+    return this.authService.updateUser(userEmail, updateUserDto);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
-    cors:{
-      origin: process.env.allowedOrigin
-    }
-  } );
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.allowedOrigin,
+      credentials: true,
+    },
+  });
+  app.use(cookieParser());
   const config = new DocumentBuilder()
-  .setTitle('Ocso API')
-  .setDescription('API for acso management')
-  .setVersion('0.9')
-  .addBearerAuth()
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }))
+    .setTitle("Ocso API")
+    .setDescription("Api for ocso managment")
+    .setVersion("0.9")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
   await app.listen(4000);
 }
 bootstrap();
